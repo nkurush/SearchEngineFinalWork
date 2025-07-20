d8b62afd19a604c528f8e237b9846760e72bfa31
kurush <nazrovkurush@gmail.com>
2025-07-21 00:31:08 +0500

    commit

---
+++ src/main/java/searchengine/model/SitePage.java
@@ -0,0 +1,41 @@
+package searchengine.model;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Entity
+@Table(name = "site")
+@NoArgsConstructor(force = true)
+@Setter
+@Getter
+public class SitePage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    private int id;
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Status status;
+    @Column(name = "status_time")
+    private Timestamp statusTime;
+    @Column(name = "last_error")
+    private String lastError = null;
+    @NotNull
+    @Column(columnDefinition = "VARCHAR(255)")
+    private String url;
+    @NotNull
+    @Column(columnDefinition = "VARCHAR(255)")
+    private String name;
+    @OneToMany(orphanRemoval = true)
+    @JoinColumn(name = "site_id")
+    private List<Page> pages;
+    @OneToMany(orphanRemoval = true)
+    @JoinColumn(name = "site_id")
+    private List<Lemma> lemmas;
+}
